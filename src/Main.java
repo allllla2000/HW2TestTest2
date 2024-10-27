89064a94e60121da1581278712a8f32b56f7f12a
allllla2000 <intergalacticwoman@gmail.com>
2024-10-27 13:16:54 +0300

    hw2testtest second commit (GI added)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("changes for the new branch  - BRANCH1 - made");
     }
 }
